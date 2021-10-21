96bc623f49ecd6e6247c33081762d140f0a8b294
jfarr-coder <jfarr001@odu.edu>
2021-10-20 22:17:50 -0400

    The Program is Able to Parse Image Files

--- PaintingMimic.py
+++ PaintingMimic.py
@@ -1,12 +1,37 @@
 import sys
-import matplotlib
-import skimage
+import numpy as np
+import matplotlib.cbook as cbook
+import matplotlib.image as image
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
+import os
 
 def convertToFileName(argv):
     inp= str(argv)
-    filename = "data/" + inp + ".png"
-    return filename
-    
+    cwd = os.getcwd()
+    filename = cwd + "/data/" + inp + ".jpeg"
+    return str(filename)
+
+def saveFileName(argv):
+    inp= str(argv)
+    cwd = os.getcwd()
+    filename = cwd + "/results/" + inp + ".jpeg"
+    return str(filename)
+
 if __name__ == "__main__":
     filename = convertToFileName(sys.argv[1])
+    saveLoc = saveFileName(sys.argv[1])
     print(filename)
+    img = image.imread(filename)
+    #imgplot = plt.imshow(img)
+    #imgplot.show()
+    with cbook.get_sample_data(filename) as image_file:
+        image = plt.imread(image_file)
+  
+    fig, ax = plt.subplots()
+    ax.imshow(image)
+    ax.axis('off')
+  
+    plt.title('matplotlib.pyplot.imread() function Example', fontweight ="bold")
+    plt.savefig(saveLoc)
+    plt.show()
